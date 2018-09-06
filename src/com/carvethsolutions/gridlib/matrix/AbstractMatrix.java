bdd84d69b7ae11ebf04b0f4eb5a70f40d2608b71
John Carveth <jcarv009@uottawa.ca>
2018-09-06 19:40:17 -0400

    -Fix

--- src/com/carvethsolutions/gridlib/matrix/AbstractMatrix.java
+++ src/com/carvethsolutions/gridlib/matrix/AbstractMatrix.java
@@ -219,7 +219,6 @@ public abstract class AbstractMatrix<E> implements Iterable<E>{
     public String toString() {
         StringBuilder sb = new StringBuilder();
         for (int y=0; y < this.getHeight()-1; y++) {
-        for (int y=0; y < this.getHeight()-1; y++) {
             StringBuilder row = new StringBuilder();
             for (int x = 0; x < this.getWidth()-1; x++) {
                 row.append(this.getDataInMatrix(x,y).toString());
